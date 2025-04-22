8e12e71a98f49530163dc56c06da59212398c556
Shammi Akhtar <106052839+ShammiAkhtar@users.noreply.github.com>
2025-04-23 00:31:20 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const loginModal = document.getElementById("loginModal");
+const loginBtn = document.getElementById("loginBtn");
+const logoutBtn = document.getElementById("logoutBtn");
+const close
+::contentReference[oaicite:0]{index=0}
+ 
